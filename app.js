22bda307fc9e96f691fa9c94cbc10698c1c4b11a
HEMANTH123SR <116257590+HEMANTH123SR@users.noreply.github.com>
2023-02-12 12:10:28 +0530

    main changes
    logic for search btn
    and styling and chaning all the details of page as per search

--- app.js
+++ app.js
@@ -3,18 +3,45 @@ const Base_url=`https://superheroapi.com/api.php/${superHero_Token}`
 const newBtn=document.getElementById('newHeroBtn');
 const heroImg=document.getElementById("heroImg");
 const searchBtn=document.getElementById("searchBtn");
-const searchInput=document.querySelector("input")
+const searchInput=document.querySelector("input");
+const nameH=document.getElementById('name')
+const intelligence=document.getElementById('intelligence');
+const strength=document.getElementById('strength');
+const power=document.getElementById('power');
+const publisher=document.getElementById('publisher');
+// const firstApparence=document.getElementById('firstApparence');
+const durability=document.getElementById('durability')
+const combat=document.getElementById('combat')
 
-const getRandomSuperHero=(id,name)=>{
+const getRandomSuperHero=(id)=>{
     fetch(`${Base_url}/${id}`)
     .then(response=>response.json())
-    .then(json=>heroImg.innerHTML=`<img src="${json.image.url}" width=300 height=300>`)  
+    .then(json=>{
+
+        heroImg.src=json.image.url;
+
+        nameH.textContent=json.name;
+
+        intelligence.textContent=` Intelligence:  ${json.powerstats.intelligence}`
+
+        strength.textContent=`Strength: ${json.powerstats.strength}`
+
+        power.textContent=`Power: ${json.powerstats.power}`;
+
+        publisher.textContent=`Publisher: ${json.biography.publisher}`;
+
+        durability.textContent=`Durability: ${json.powerstats.durability}`;
+
+        combat.textContent=`Combat: ${json.powerstats.combat}`
+        // firstApparence.textContent;
+
+    
+    })  
 }
 
 newBtn.onclick=()=>{
     let ran=Math.floor(Math.random()*731)+1
     getRandomSuperHero(ran)
-    console.log(ran)
 }
 
 const getSearchSuperHero=(name)=>{
@@ -23,13 +50,29 @@ const getSearchSuperHero=(name)=>{
     .then(json=>{
          const hero=json.results[0]
          console.log(hero)
-        heroImg.innerHTML=`<img src="${hero.image.url}" width=300 height=300>`})  
+
+        // heroImg.innerHTML=`<img src="${hero.image.url}" width=300 height=300>`})  
+        heroImg.src=hero.image.url;
+
+        nameH.textContent=hero.name;
+
+        intelligence.textContent=` Intelligence:  ${hero.powerstats.intelligence}`
+
+        strength.textContent=`Strength: ${hero.powerstats.strength}`
+
+        power.textContent=`Power: ${hero.powerstats.power}`;
+
+        publisher.textContent=`Publisher: ${hero.biography.publisher}`;
+
+        durability.textContent=`Durability: ${hero.powerstats.durability}`;
+
+        combat.textContent=`Combat: ${hero.powerstats.combat}`
+})
 }
 
 
 searchBtn.onclick=()=>{
     getSearchSuperHero(searchInput.value);    
 }
-
 
 
